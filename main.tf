bf67f8362c8b5e5ee7c8d7c20ef3627ca477da9f
Mohamed Tharik <94281905+tharik-109@users.noreply.github.com>
2025-03-22 13:25:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Create VPC
 resource "aws_vpc" "mongodb" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "172.31.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -16,44 +16,44 @@ resource "aws_vpc" "mongodb" {
 # Create Public Subnets
 resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.mongodb.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "172.31.32.0/20"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "Public-1"
   }
 }
 
 resource "aws_subnet" "public2" {
   vpc_id                  = aws_vpc.mongodb.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "172.31.0.0/20"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 
   tags = {
-    Name = "public-subnet-2"
+    Name = "Public-2"
   }
 }
 
 # Create Private Subnets
 resource "aws_subnet" "private1" {
   vpc_id            = aws_vpc.mongodb.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "172.31.16.0/20"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "private-subnet-1"
+    Name = "Private-1"
   }
 }
 
 resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.mongodb.id
-  cidr_block        = "10.0.4.0/24"
+  cidr_block        = "172.31.8.0/20"
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "private-subnet-2"
+    Name = "Private-2"
   }
 }
 
